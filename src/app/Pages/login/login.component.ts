2a9da90523f290a1a20a54f6b9cd33bd36295711
Yousef Abdelrhem <73524516+Yousef-Abdelrhem@users.noreply.github.com>
2025-02-22 16:16:04 +0200

    building login and signup and vaildate them

--- src/app/Pages/login/login.component.ts
+++ src/app/Pages/login/login.component.ts
@@ -1,9 +1,33 @@
 import { Component } from '@angular/core';
 import { ButtonComponent } from '../../Components/button/button.component';
+import { RouterLink } from '@angular/router';
+import {FormGroup , FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
     selector: 'app-login',
-    imports: [ButtonComponent],
+    imports: [ButtonComponent, RouterLink, ReactiveFormsModule, CommonModule],
     templateUrl: './login.component.html',
 })
-export class LoginComponent {}
+export class LoginComponent {
+    eye = 'icons/eye.svg';    
+    passType = 'password';
+    form = new FormGroup({
+        email : new FormControl('', [Validators.required, Validators.email]),
+        password : new FormControl('',[Validators.required, Validators.minLength(9)])
+    })
+
+    
+    get email(){
+        return this.form.get('email');
+    }
+    get password(){
+        return this.form.get('password');
+    }
+    
+
+    changIcon(){
+        this.passType = (this.passType == 'password' ? 'text' : 'password');
+        this.eye = (this.eye == 'icons/eye.svg' ?'icons/hideEye.svg' : 'icons/eye.svg');
+    }
+}
--- src/app/Pages/register/register.component.ts
+++ src/app/Pages/register/register.component.ts
@@ -1,8 +1,42 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { RouterLink } from '@angular/router';
 
 @Component({
     selector: 'app-register',
-    imports: [],
+    imports: [RouterLink, CommonModule,ReactiveFormsModule, ],
     templateUrl: './register.component.html',
 })
-export class RegisterComponent {}
+export class RegisterComponent {
+        eye = 'icons/eye.svg';    
+        passType = 'password';
+      
+        form = new FormGroup({
+            name: new FormControl('', [Validators.required, Validators.minLength(3)]),
+            email : new FormControl('', [Validators.required, Validators.email]),
+            password : new FormControl('',[Validators.required, Validators.minLength(9)]),
+            confirmPassword : new FormControl('',[Validators.required, Validators.minLength(9)])
+        })
+
+        get name(){
+            return this.form.get('name');
+        }
+        get email(){
+            return this.form.get('email');
+        }
+        get password(){
+            return this.form.get('password');
+        }
+        get confirmPassword(){
+            return this.form.get('confirmPassword');
+        }
+    
+        changIcon(){
+            this.passType = (this.passType == 'password' ? 'text' : 'password');
+            this.eye = (this.eye == 'icons/eye.svg' ?'icons/hideEye.svg' : 'icons/eye.svg');
+        //    console.log(this.form.get('name'));
+
+        }
+        
+}
