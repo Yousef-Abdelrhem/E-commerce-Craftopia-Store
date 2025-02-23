9401bb16a9590d1d07760ce53b1d9dbadba0deb0
rehamsaed <rsaed4214@gmail.com>
2025-02-23 17:00:44 +0200

    Our Reviews section added

--- src/app/Pages/home/home.component.ts
+++ src/app/Pages/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { FirstSectionComponent } from '../../Components/first-section/first-section.component';
 import { SecondSectionComponent } from '../../Components/second-section/second-section.component';
+import { OurReviewsComponent } from '../../Components/our-reviews/our-reviews.component';
 
 @Component({
     selector: 'app-home',
-    imports: [FirstSectionComponent,SecondSectionComponent],
+    imports: [FirstSectionComponent,SecondSectionComponent,OurReviewsComponent],
     templateUrl: './home.component.html',
     styles: ``,
 })
