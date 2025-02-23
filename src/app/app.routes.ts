0968a52eb70a899f5b62d983da57068706810fbd
Yousef Abdelrhem <73524516+Yousef-Abdelrhem@users.noreply.github.com>
2025-02-23 16:05:08 +0200

    building trending section

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './Pages/login/login.component';
 import { RegisterComponent } from './Pages/register/register.component';
+import { TrendingComponent } from './Pages/trending/trending.component';
 
 export const routes: Routes = [
     { path: 'login', component: LoginComponent },
     { path: 'register', component: RegisterComponent },
+    { path: "trending" , component: TrendingComponent}
 ];
