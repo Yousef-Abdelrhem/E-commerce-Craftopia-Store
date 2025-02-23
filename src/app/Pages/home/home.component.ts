f0c0338ba6d3c551b12a36c27ff2d25959ebf1c8
dina <dina2022001@gmail.com>
2025-02-23 17:36:54 +0200

    update Images

--- src/app/Pages/home/home.component.ts
+++ src/app/Pages/home/home.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { FirstSectionComponent } from '../../Components/first-section/first-section.component';
 import { SecondSectionComponent } from '../../Components/second-section/second-section.component';
+import { AboutUsComponent } from '../../Components/about-us/about-us.component';
+import { TopCategoriesComponent } from '../../Components/top-categories/top-categories.component';
 
 @Component({
     selector: 'app-home',
-    imports: [FirstSectionComponent,SecondSectionComponent],
+    imports: [FirstSectionComponent,SecondSectionComponent,AboutUsComponent,TopCategoriesComponent],
     templateUrl: './home.component.html',
     styles: ``,
 })
