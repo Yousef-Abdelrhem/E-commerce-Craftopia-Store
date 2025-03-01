133fca0d6f2e173d73be8d2d0ef75bd370b17098
dina <dina2022001@gmail.com>
2025-03-01 12:52:54 +0200

    some changes in home page

--- src/app/Pages/home/home.component.ts
+++ src/app/Pages/home/home.component.ts
@@ -2,10 +2,13 @@ import { Component } from '@angular/core';
 import { FirstSectionComponent } from '../../Components/first-section/first-section.component';
 import { SecondSectionComponent } from '../../Components/second-section/second-section.component';
 import { OurReviewsComponent } from '../../Components/our-reviews/our-reviews.component';
+import { AboutUsComponent } from '../../Components/about-us/about-us.component';
+import { TopCategoriesComponent } from '../../Components/top-categories/top-categories.component';
+import { TrendingComponent } from '../trending/trending.component';
 
 @Component({
     selector: 'app-home',
-    imports: [FirstSectionComponent,SecondSectionComponent,OurReviewsComponent],
+    imports: [FirstSectionComponent,SecondSectionComponent, AboutUsComponent, TopCategoriesComponent,TrendingComponent, OurReviewsComponent],
 
     templateUrl: './home.component.html',
     styles: ``,
