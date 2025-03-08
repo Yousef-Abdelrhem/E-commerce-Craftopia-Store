297ea96816053c2249c5e5054c6a98faa50e8a63
rehamsaed <rsaed4214@gmail.com>
2025-03-08 11:24:10 +0200

    cart added

--- src/app/Components/header/header.component.ts
+++ src/app/Components/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { CartServiceService } from '../../Services/cart-service.service';
 
 @Component({
     selector: 'app-header',
@@ -6,4 +7,11 @@ import { Component } from '@angular/core';
     templateUrl: './header.component.html',
     styleUrl: './header.component.css',
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+    constructor(private cartService: CartServiceService) {}
+
+    toggleCart(event: Event) {
+        event.preventDefault();
+        this.cartService.toggleCart();
+    }
+}
