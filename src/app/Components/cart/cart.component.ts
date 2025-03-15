9de61e7db4062354d253e8e1b3dbcce9ef6e49fa
rehamsaed <rsaed4214@gmail.com>
2025-03-15 11:42:52 +0200

    Cart functionality added

--- src/app/Components/cart/cart.component.ts
+++ src/app/Components/cart/cart.component.ts
@@ -4,12 +4,50 @@ import { CartServiceService } from '../../Services/cart-service.service';
 
 @Component({
     selector: 'app-cart',
+    standalone: true,
     imports: [CommonModule],
     templateUrl: './cart.component.html',
     styleUrl: './cart.component.css',
 })
 export class CartComponent implements OnInit {
     isCartVisible = false;
+    cartItems = [
+        {
+            name: 'Pure Chakra',
+            price: 350.0,
+            oldPrice: 400.0,
+            quantity: 1,
+            image: 'Bag.png',
+        },
+        {
+            name: 'Pure Chakra',
+            price: 350.0,
+            oldPrice: 400.0,
+            quantity: 1,
+            image: 'Bag.png',
+        },
+        {
+            name: 'Pure Chakra',
+            price: 350.0,
+            oldPrice: 400.0,
+            quantity: 1,
+            image: 'Bag.png',
+        },
+        {
+            name: 'Pure Chakra',
+            price: 350.0,
+            oldPrice: 400.0,
+            quantity: 1,
+            image: 'Bag.png',
+        },
+        {
+            name: 'Pure Chakra',
+            price: 350.0,
+            oldPrice: 400.0,
+            quantity: 1,
+            image: 'Bag.png',
+        },
+    ];
 
     constructor(private cartService: CartServiceService) {}
 
@@ -21,5 +59,25 @@ export class CartComponent implements OnInit {
 
     closeCart() {
         this.cartService.toggleCart();
+    }
+
+    increaseQuantity(item: any) {
+        item.quantity++;
+    }
+
+    decreaseQuantity(item: any) {
+        if (item.quantity > 1) {
+            item.quantity--;
+        }
+    }
+
+    removeItem(index: number) {
+        this.cartItems.splice(index, 1);
+    }
+
+    getTotalPrice(): string {
+        return `${this.cartItems
+            .reduce((total, item) => total + item.price * item.quantity, 0)
+            .toFixed(2)}`;
     }
 }
